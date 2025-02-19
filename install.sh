3cb598a04384307191cd6e3666134d90a217755e
Avi Kadria <avi.kadria3@gmail.com>
2025-02-19 22:42:57 +0200

    cleaned the install and start scripts :)

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -keyout key.pem -out cert.pem
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -keyout key.pem -out cert.pem -subj "/CN=localhost"
+sudo rm /usr/local/share/ca-certificates/my-cert.crt
+sudo rm /etc/ssl/certs/my-cert.pem
 sudo cp cert.pem /usr/local/share/ca-certificates/my-cert.crt
 sudo update-ca-certificates
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
+sudo killall python3
 sudo python3 -m http.server 80 >/dev/null 2>/dev/null &
 sudo python3 simple_https_server.py >/dev/null 2>/dev/null &
